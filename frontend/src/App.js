8291d50621fc8a1fe0bfc06db91c55ea00914d4d
CreeperSama <nathayush36@gmail.com>
2026-06-11 17:23:46 +0530

    Backend Depployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -107,7 +107,7 @@ function App() {
     setLoading(true);
     setResult(null); 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/predict', formData);
+      const response = await axios.post(' https://churn-backend-nb7j.onrender.com', formData);
       setResult(response.data);
     } catch (error) {
       if (error.response && error.response.data && error.response.data.error) {
